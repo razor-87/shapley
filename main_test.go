4db5cebf5096d68cffc67c9e65eab726736858ad
Sergei Razorenov <sergrazor87@gmail.com>
2022-12-05 03:52:00 +0300

    add tests

---
+++ main_test.go
@@ -0,0 +1,126 @@
+package main
+
+import (
+	"io"
+	"math"
+	"reflect"
+	"strings"
+	"testing"
+)
+
+const data = `
+Google;0.18
+Meta;0.04
+Microsoft;0.08
+Meta,Google;0.1
+Microsoft,Google;0.26
+Meta,Microsoft;0.07
+Meta,Microsoft,Google;0.27
+`
+
+func mockRecords() [][]string {
+	return [][]string{{"Google", "0.18"}, {"Meta", "0.04"}, {"Microsoft", "0.08"}, {"Meta,Google", "0.1"}, {"Microsoft,Google", "0.26"}, {"Meta,Microsoft", "0.07"}, {"Meta,Microsoft,Google", "0.27"}}
+}
+
+func mockChannels() []string {
+	return []string{"Google", "Meta", "Microsoft"}
+}
+
+func mockWorths() map[string]float64 {
+	return map[string]float64{"Google": 0.18, "Google Meta": 0.32, "Google Meta Microsoft": 1, "Google Microsoft": 0.52, "Meta": 0.04, "Meta Microsoft": 0.19, "Microsoft": 0.08}
+}
+
+func Test_prepare(t *testing.T) {
+	type args struct {
+		r io.Reader
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    [][]string
+		wantErr bool
+	}{
+		{
+			name: "simple",
+			args: args{r: strings.NewReader(data)},
+			want: mockRecords(),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := prepare(tt.args.r)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("prepare() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("prepare() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_handle(t *testing.T) {
+	type args struct {
+		records [][]string
+	}
+	tests := []struct {
+		name         string
+		args         args
+		wantChannels []string
+		wantWorths   map[string]float64
+		wantErr      bool
+	}{
+		{
+			name:         "simple",
+			args:         args{mockRecords()},
+			wantChannels: mockChannels(),
+			wantWorths:   mockWorths(),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotChannels, gotWorths, err := handle(tt.args.records)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("handle() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(gotChannels, tt.wantChannels) {
+				t.Errorf("handle() gotChannels = %v, want %v", gotChannels, tt.wantChannels)
+			}
+			for key, value := range gotWorths {
+				if wantValue := tt.wantWorths[key]; math.Abs(wantValue-value) > 1e-9 {
+					t.Errorf("wantValue = %v, gotValue = %v", wantValue, value)
+				}
+			}
+		})
+	}
+}
+
+func Test_shapley(t *testing.T) {
+	type args struct {
+		channels []string
+		worths   map[string]float64
+	}
+	tests := []struct {
+		name string
+		args args
+		want map[string]float64
+	}{
+		{
+			name: "simple",
+			args: args{channels: mockChannels(), worths: mockWorths()},
+			want: map[string]float64{"Google": 0.45, "Meta": 0.215, "Microsoft": 0.335},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := shapley(tt.args.channels, tt.args.worths)
+			for key, value := range got {
+				if wantValue := tt.want[key]; math.Abs(wantValue-value) > 1e-9 {
+					t.Errorf("wantValue = %v, gotValue = %v", wantValue, value)
+				}
+			}
+		})
+	}
+}
