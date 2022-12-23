d3938a3dfecfaa0faffe67fe3ee33a40736ebe64
Sergei Razorenov <sergrazor87@gmail.com>
2022-12-23 23:41:00 +0300

    fix naming, simplify concurrency

--- main.go
+++ main.go
@@ -20,15 +20,7 @@ import (
 	"golang.org/x/exp/slices"
 )
 
-const (
-	limitSinkBuffer = 1
-	limitWorkers    = 2
-)
-
-type sValues struct {
-	values map[string]float64
-	mu     sync.Mutex
-}
+const epsilon = 1e-9
 
 var (
 	cpuprofile = flag.Bool("cpuprofile", false, "write cpu profile to cpu.prof")
@@ -132,12 +124,12 @@ func calc() (map[string]float64, error) {
 		return nil, fmt.Errorf("failed to prepare data, %w", err)
 	}
 
-	channels, worths, err := handle(records)
+	players, worths, err := handle(records)
 	if err != nil {
 		return nil, fmt.Errorf("failed to handle data, %w", err)
 	}
 
-	sValues, checkSum := shapley(channels, worths)
+	sValues, checkSum := shapley(players, worths)
 	if notEqualsOne(checkSum) {
 		return nil, fmt.Errorf("sum of Shapley values isn't equal to one, %v", checkSum)
 	}
@@ -159,7 +151,7 @@ func prepare(r io.Reader, g int) ([][]string, error) {
 	return records, nil
 }
 
-func handle(records [][]string) (channels []string, worths map[string]float64, err error) {
+func handle(records [][]string) (players []string, worths map[string]float64, err error) {
 	set := make(map[string]struct{})
 	cValues := make(map[string]float64, len(records))
 	for _, rec := range records {
@@ -177,11 +169,11 @@ func handle(records [][]string) (channels []string, worths map[string]float64, e
 		cValues[coalition] = val
 	}
 
-	channels = make([]string, 0, len(set))
-	for channel := range set {
-		channels = append(channels, channel)
+	players = make([]string, 0, len(set))
+	for player := range set {
+		players = append(players, player)
 	}
-	sort.Strings(channels)
+	sort.Strings(players)
 
 	worths = make(map[string]float64, len(records))
 	for coalition := range cValues {
@@ -192,7 +184,7 @@ func handle(records [][]string) (channels []string, worths map[string]float64, e
 		}
 	}
 
-	return channels, worths, nil
+	return players, worths, nil
 }
 
 func containsAll(coalition, c string) bool {
@@ -205,111 +197,88 @@ func containsAll(coalition, c string) bool {
 	return true
 }
 
-func shapley(channels []string, worths map[string]float64) (map[string]float64, float64) {
-	n := len(channels)
-	svs := &sValues{values: make(map[string]float64, n)}
+func shapley(players []string, worths map[string]float64) (map[string]float64, float64) {
+	n := len(players)
+	vector := make([]float64, n)
 
-	sets := make([]chan []int, n)
-	for g := 0; g < n; g++ {
-		sets[g] = make(chan []int, limitSinkBuffer)
+	powersets := make([]chan []int, n)
+	buffer := 1 << (n / 2)
+	for ch := 0; ch < n; ch++ {
+		powersets[ch] = make(chan []int, buffer)
 	}
 
 	go func() {
-		makeSubsetsIdxs(n, sets)
+		makeSubsetsIdxs(n, powersets)
 	}()
 
 	var wgg sync.WaitGroup
 	wgg.Add(n)
-	for i, channel := range channels {
-		go func(i int, channel string) {
+	for i, player := range players {
+		go func(i int, player string) {
 			defer wgg.Done()
 
-			chW := make(chan float64, 1)
-			var wg sync.WaitGroup
-			wg.Add(limitWorkers)
-			for l := 0; l < limitWorkers; l++ {
-				go func() {
-					defer wg.Done()
+			var pSum float64
+			for idxs := range powersets[i] {
+				if slices.Contains(idxs, i) {
+					continue
+				}
 
-					for subsetIdxs := range sets[i] {
-						if slices.Contains(subsetIdxs, i) {
-							continue
-						}
+				S := make([]string, len(idxs))
+				for ii := range idxs {
+					S[ii] = players[idxs[ii]]
+				}
+				k := len(S)
+				A := strings.Join(S, " ")
+				Si := S
+				Si = append(Si, player)
+				sort.Strings(Si)
+				Ai := strings.Join(Si, " ")
 
-						S := make([]string, len(subsetIdxs))
-						for ii := range subsetIdxs {
-							S[ii] = channels[subsetIdxs[ii]]
-						}
-						k := len(S)
-						A := strings.Join(S, " ")
-						Si := S
-						Si = append(Si, channel)
-						sort.Strings(Si)
-						Ai := strings.Join(Si, " ")
+				nominator := factorial(k) * factorial(n-k-1)
+				denominator := factorial(n)
+				// Weight = |S|!(n-|S|-1)!/n!
+				weight := nominator / denominator
+				// Marginal contribution = v(S U {i})-v(S)
+				contrib := worths[Ai] - worths[A]
 
-						nominator := factorial(k) * factorial(n-k-1)
-						denominator := factorial(n)
-						// Weight = |S|!(n-|S|-1)!/n!
-						weight := nominator / denominator
-						// Marginal contribution = v(S U {i})-v(S)
-						contrib := worths[Ai] - worths[A]
-
-						chW <- weight * contrib
-					}
-				}()
+				pSum += weight * contrib
 			}
 
-			chSum := make(chan float64)
-			go func() {
-				var sum float64
-				for sValue := range chW {
-					sum += sValue
-				}
-				chSum <- sum
-			}()
-
-			wg.Wait()
-			close(chW)
-
-			s := <-chSum
-
-			svs.mu.Lock()
-			svs.values[channel] = s + worths[channel]/float64(n)
-			svs.mu.Unlock()
-
-		}(i, channel)
+			vector[i] = pSum + worths[player]/float64(n)
+		}(i, player)
 	}
 	wgg.Wait()
 
-	var sum float64
-	for _, value := range svs.values {
-		sum += value
+	var vSum float64
+	sValues := make(map[string]float64, n)
+	for i, value := range vector {
+		vSum += value
+		sValues[players[i]] = value
 	}
 
-	return svs.values, sum
+	return sValues, vSum
 }
 
-func makeSubsetsIdxs(n int, sets []chan []int) {
+func makeSubsetsIdxs(n int, powersets []chan []int) {
 	max := (1 << n) - 1
 
 	go func() {
 		defer func() {
-			for _, set := range sets {
-				close(set)
+			for _, powerset := range powersets {
+				close(powerset)
 			}
 		}()
 
-		for binNum := 1; binNum <= max; binNum++ { // if binNum := 1 that without a null set
-			var subsetIdxs []int
+		for bin := 1; bin <= max; bin++ { // if bin := 1 that without a null set
+			var subsetIndexes []int
 			for i := 0; i < n; i++ {
-				if (1<<i)&binNum > 0 {
-					subsetIdxs = append(subsetIdxs, i)
+				if (1<<i)&bin > 0 {
+					subsetIndexes = append(subsetIndexes, i)
 				}
 			}
-			if subsetIdxs != nil {
-				for _, set := range sets {
-					set <- subsetIdxs
-				}
+
+			for _, powerset := range powersets {
+				powerset <- subsetIndexes
 			}
 		}
 	}()
@@ -323,5 +292,5 @@ func factorial(n int) float64 {
 }
 
 func notEqualsOne(f float64) bool {
-	return math.Abs(f-1) > 1e-9
+	return math.Abs(f-1) > epsilon
 }
